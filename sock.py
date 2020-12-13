af51c29b648b683ddb217da7b2d1c7ab2c74c480
atHome <cloudonly21@gmail.com>
2020-12-13 22:46:14 +0900

    add socket.py, update .gitignore

---
+++ sock.py
@@ -0,0 +1,13 @@
+import socket
+
+with socket.socket() as s:
+    s.bind(('localhost', 8000))
+    s.listen(1)
+    conn, addr = s.accept()
+
+    while True:
+        with conn:
+            request = conn.recv(1024).decode('utf-8')
+            print(request)
+            # conn.sendall('Hello World'.encode('utf-8'))
+            break
